f1f54f1ca7fde666bba99255184011a84fca0b43
bedrierol33 <bedri33canerol@gmail.com>
2022-04-20 00:30:28 +0300

    selenium
    ::::::::: selenium :::::::::

---
+++ selenium/main.py
@@ -0,0 +1,18 @@
+import time
+
+import selenium
+from selenium import webdriver
+url=input("url girin: ")
+browser=webdriver.Chrome()
+#browser.get(url)            #yazılan url ye gider
+#print(browser.title)        #sayfanın title ını yazdırır
+#browser.refresh()           #sayfa yenileme
+#browser.back()              #önceki sayfaya döner
+#browser.close()             #sekmeyi kapatır
+#browser.maximize_window()   #tam ekran yapar
+#browser.get_window_size()   #açıklamaya gerek yok anladınız ne olduğunu
+#browser.quit()              #chrome u kapatır
+#print(browser.page_source)  #sayfa kodlarını yazdırır
+#browser.save_screenshot()   #ekran görüntüsü alır
+#print(browser.current_url)  #bulunduğu sayfanın urlsini yazdırır
+
